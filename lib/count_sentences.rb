d4d8a17607bea76711a37858a99d250a5c318c5c
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-25 18:25:18 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter = 0
+      self.split.map {|x|
+        if x.squeeze.sentence? || x.squeeze.question? || x.squeeze.exclamation?
+          counter += 1
+        end
+      }
+      counter
   end
 end
